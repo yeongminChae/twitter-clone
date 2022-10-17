ef516f52c5d7e971ca5eda5683da24d5032b513b
Yeongmin_Chae <codudals98@naver.com>
2022-10-17 17:42:13 +0900

    ch2 Authentication : Using Firebase auth

--- javascript/nwitter/src/components/App.js
+++ javascript/nwitter/src/components/App.js
@@ -1,7 +1,15 @@
-import Router from "./Router";
+import React, { useState } from "react";
+import fBase, { authService } from "fBase";
+import AppRouter from "components/Router";
 
 function App() {
-  return <Router />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
+    </>
+  );
 }
 
 export default App;
--- javascript/nwitter/src/fBase.js
+++ javascript/nwitter/src/fBase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -9,5 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
+export const authService = getAuth();
 export default app;
