cb1cb52a6afc40a1886cf9fc6d2b23fdfe12dcf2
Yeongmin_Chae <codudals98@naver.com>
2022-10-17 16:49:32 +0900

    ch1 Setup : React + Firebase Setup

---
+++ javascript/nwitter/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAgQQ99a8BALm-g2iSU6CMTz7sLKVW9UYQ",
+  authDomain: "neither-c4cc4.firebaseapp.com",
+  projectId: "neither-c4cc4",
+  storageBucket: "neither-c4cc4.appspot.com",
+  messagingSenderId: "171082847708",
+  appId: "1:171082847708:web:e459509f8a08c4fdef2b19",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
