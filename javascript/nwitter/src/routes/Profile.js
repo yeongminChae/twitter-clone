612d50ae67794843078353bf41adf7ea6e3295d0
Yeongmin_Chae <codudals98@naver.com>
2022-10-19 17:54:09 +0900

    ch5 Edit Profile : Get My Own Nweets

--- javascript/nwitter/src/routes/Profile.js
+++ javascript/nwitter/src/routes/Profile.js
@@ -1,13 +1,28 @@
-import { authService } from "fBase";
-import React from "react";
+import { authService, dbService } from "fBase";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 
-function Profile() {
+function Profile({ userObj }) {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getMyNweets = async () => {
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt", "desc")
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, "=>", doc.data());
+    });
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
   return (
     <div>
       Profile
